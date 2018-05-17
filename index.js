97495a183db0825390092bb0c4310471393443ea
el-davo <david.ahern26@gmail.com>
2018-05-17 13:55:07 +0100

    Updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {graphiqlExpress, graphqlExpress} = require('apollo-server-express/dist');
 const bodyParser = require('body-parser');
 const express = require('express');
+const {apolloUploadExpress} = require('apollo-upload-server');
 
 const port = process.env.PORT || 3030;
 const app = express();
@@ -8,10 +9,14 @@ const endpointURL = '/graphql';
 
 const schema = require('./lib/schema');
 
-app.use(endpointURL, bodyParser.json(), graphqlExpress(request => ({
-    schema,
-    context: request.headers.authorization
-})));
+app.use(endpointURL,
+    bodyParser.json(),
+    apolloUploadExpress(/* Options */),
+    graphqlExpress(request => ({
+        schema,
+        context: request.headers.authorization
+    }))
+);
 app.use('/graphiql', graphiqlExpress({endpointURL}));
 
 app.listen(port, () =>
